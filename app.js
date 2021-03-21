c2c0adc71e31ec1a0125ee90a72c29f1b53dfe97
Marsha Hall <mphall77@gmail.com>
2021-03-21 02:10:16 -0400

    up to click image

---
+++ app.js
@@ -0,0 +1,42 @@
+//  **** N O T E S ****
+// https://rickandmortyapi.com/api
+// {
+//   "characters": "https://rickandmortyapi.com/api/character",
+//   "locations": "https://rickandmortyapi.com/api/location"
+// }
+
+// https://www.educative.io/edpresso/how-to-add-an-id-to-element-in-javascript
+// ASSIGN AN ID ==> TAG/VARIABLE.setAttribute(‘id’,‘ID-NAME’);
+// ADD MULTIPLE ATTRIBUTES ==> TAG/VARIABLE.setAttribute(‘class’, ‘CLASS-NAME’);
+
+// get all the characters from the first page of the API as soon s the page loads ==> DOMContentLoaded
+document.addEventListener("DOMContentLoaded", async (e) => {
+	try {
+		const characters = await axios.get(
+			"https://rickandmortyapi.com/api/character"
+			// "https://rickandmortyapi.com/api/character/?page=1"
+		);
+		const ul = document.querySelector("ul");
+
+		// loop the characters to get data for each
+		characters.data.results.forEach((character) => {
+			// create the li
+			const li = document.createElement("li");
+			li.setAttribute("id", "photo-img");
+			// image
+			const img = document.createElement("img");
+			img.src = character.image;
+			li.appendChild(img);
+			// name
+			const p = document.createElement("p");
+			p.setAttribute("id", "character-name");
+			p.textContent = character.name;
+			li.appendChild(p);
+			ul.appendChild(li);
+		});
+		// status
+		// location
+	} catch (err) {
+		console.log(err);
+	}
+});
