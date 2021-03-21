1ac237c6f09c61571db95cef53d28e25e09d8f11
Coreen Cooper <coreencooper@pursuit.org>
2021-03-21 11:36:13 -0400

    add instructions to js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+// click on a character.
+// When they select a character, the `main` should appear and information about that
+// character (name, image, status, location name) should populate in the `character-info` section.
+// When they click on a different character, this information should be replaced.
+// The page `title` should also match the name of the character selected. 
+// Use the `form`, to submit a character comment. Each comment should be a new `li` inside of
+// `character-comments-ul`, with the selected character's name (in bold) and a comment (not bold).
+
+
 fetch("https://rickandmortyapi.com/api/character?page=1")
   .then((res) => {
     if (!res.ok) {
@@ -14,6 +23,7 @@ fetch("https://rickandmortyapi.com/api/character?page=1")
       const p = document.createElement("p");
       p.textContent = char["name"]; // display name
       img.src = char["image"]; // display image
+      li.classList.add("photo-img"); 
       ul.appendChild(li);
       li.appendChild(img);
       li.appendChild(p);
