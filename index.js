57a98fc6eee78311bcfc380d96e75206468a3bed
Alexander <alexanderl96@yahoo.com>
2021-03-23 15:26:07 -0400

    fsd

--- index.js
+++ index.js
@@ -1,84 +1,66 @@
-document.addEventListener('DOMContentLoaded', e => {
-    e.preventDefault()
-    const ul = document.getElementById('all-characters')
-    let count = []
+    const allChar = document.getElementById('all-characters')
 
     async function char() {
         const result =  await axios.get("https://rickandmortyapi.com/api/character")
         console.log(result.data.results)
         result.data.results.forEach(el => {
-            console.log(el)
-            count.push(el)
-
-
-
-            let card = document.createElement('div')
-            if(el.image > 5){
-                card.setAttribute('style', 'width: 358px')
-            }
-            card.textContent = ""
-            const li = ul.appendChild(document.createElement('li'))
-            const img = li.appendChild(document.createElement('img'))
-            img.setAttribute('src', el.image )
-            img.setAttribute('style','higth: 20px')
-            card.appendChild(img)
+            const li2 = document.createElement('li')
+            li2.classList.add('photo')
+            const img = document.createElement('img')
+            img.classList.add('photo-img')
             img.src = el.image
-    
-
-
-
-
+            const ptwo = document.createElement('p') 
+            ptwo.textContent = el.name
+            li2.appendChild(img)
+            li2.appendChild(ptwo)
+            allChar.appendChild(li2) 
         });
-    
-    
+
+    }
+    char()
+
+    function caracter(all){
         
     }
 
-   
-
-   
-
-    char()
-})
-
-
-// console.log(el.name)
-// let {name, status} = result.data.results[0]
-// let{name: location}= result.data.results[0].origin
-// let img = result.data.results[0].image
-// console.log(name)
-// console.log(status)
-// console.log(location)
-// console.log(img)
-
-// 
-// 
-// let photo =document.getElementById('photo')
-// photo.textContent =  `<img src="assets/1.jpeg">`
-// 
-
-
-
-
-// let info = document.getElementById('character-info')
-//         let h3 = document.querySelector('h3')
-//         h3.textContent = name
-//         let first = document.getElementById('first')
-//         first.textContent = ` Status: ${status}`
-//         let second = document.getElementById('second')
-//         second.textContent = `Location: ${location}`
-//         info.appendChild(h3, photo,  first, second)
+    allChar.addEventListener('click', e =>{
+        e.preventDefault()
+        fetch(`https://rickandmortyapi.com/api/character/${e.target.value}`)
+        .then((res)=>{
+            return res.json()
+        }).then((res)=>{
+            let todo = res.data
+            console.log(todo.name)
+            const main = document.querySelector("main");
+            main.style.display = "flex";
+            const title = document.querySelector("title");
+            title.textContent = res.name;
+            const name = document.querySelector('h3')
+            name.textContent = res.data.name
+            const status = document.getElementById('first')
+            status.innerHTML = `Status: ${res.status}`
+            const location = document.getElementById('second')
+            location.innerHTML = `Location: ${res.location.name}`
+            const img = document.getElementById('photo')
+            img.src = res.image
+        })
         
-//         const form = document.querySelector('form')
-//         const input = document.querySelector('input')
-//         const ul2 = document.getElementById('character-comments-ul')
-//         const button = document.querySelector('button')
-//         button.addEventListener('submit', e =>{
-//             e.preventDefault()
-//             let li = document.createElement('li')
-//             li.textContent = input.value
-//             ul2.appendChild(li)
-            
-//         })
-//         form.appendChild(ul2)
-      
+    })
+    const form = document.querySelector("form");
+
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const comments = document.querySelector("#character-comments-ul");
+  const input = document.querySelector("input[type=text]");
+  const title = document.querySelector("title");
+  const li = document.createElement("li");
+  li.innerHTML = `<b>${title.textContent}:</b> ${input.value}`;
+  comments.appendChild(li);
+  input.value = "";
+});
+
+
+
+
+
