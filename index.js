a2746bd657d5497fac5589fe2a0424b9e43b2dee
anjuhypp <anjuhypp@gmail.com>
2021-03-22 20:44:42 -0400

    practice assessment I

--- index.js
+++ index.js
@@ -1,60 +1,53 @@
-//`li`'s with an image (id=`photo-img`) of each character in the API (first page only), as well as 
-// the characters name.
-console.log("WTF?!")
+const allCharacters = document.querySelector("#all-characters");
+const main = document.querySelector("main");
+const name = document.querySelector("#character-name");
+const image = document.querySelector("#character-img");
+const status = document.querySelector("#character-status");
+const place = document.querySelector("#character-location");
+const title = document.querySelector("title");
+const form = document.querySelector("form")
+const comments = document.querySelector("#character-comments-ul")
 
-const mainTag = document.createElement("main")
-const body = document.querySelector("body")
-body.appendChild(mainTag)
+const getAllCharacters = async () => {
+  try {
+    const res = await axios.get("https://rickandmortyapi.com/api/character");
+    // debugger;
+    res.data.results.forEach((character) => {
+      const li = document.createElement("li");
+      const img = document.createElement("img");
+      const p = document.createElement("p");
+      // console.log(character);
+      img.src = character.image;
+      img.value = character.id;
+      p.innerText = character.name;
+      li.appendChild(img);
+      li.appendChild(p);
+      allCharacters.appendChild(li);
+      // debugger;
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+getAllCharacters();
 
-
-const charInfo = document.createElement("section")
-charInfo.setAttribute("id", "character-info")
-mainTag.appendChild(charInfo)
-
-const charCommentSec = document.createElement("section")
-charCommentSec.setAttribute("id", "character-comments-section")
-mainTag.appendChild(charCommentSec)
-
-const h3 = document.createElement("h3")
-const h3Image = document.createElement("img")
-const pTagOne = document.createElement("p")
-const pTagTwo = document.createElement("p")
-charInfo.appendChild(h3, h3Image, pTagOne, pTagTwo)
-
-const form = document.createElement("form")
-const textInput = document.createElement("input")
-const submitInput = document.createElement("input")
-// On submission the input should clear.
-charCommentSec.appendChild(form)
-
-// const ul = document.createElement("ul")
-// ul.setAttribute("id", "character-comments-ul")
-// charCommentSec.appendChild(ul)
-
-// debugger
-fetch("https://rickandmortyapi.com/api/character")
-.then((res) => {
-    // debugger
-    if(!res.ok) throw Error(`Something smells fishy!" ${res.status}`);
-    return res.json();
-})
-.then((res) => {
-    // debugger
-    res.results.forEach((person) => {
-        const ul = document.querySelector("#all-characters")
-        const li = document.createElement("li")
-        const img = document.createElement("img")
-        li.textContent = person.name
-        img.src = person.image
-        ul.appendChild(li)
-        ul.appendChild(img)
-    })
+allCharacters.addEventListener("click", async (e) => {
+  main.style.display = "flex";
+  const url = `https://rickandmortyapi.com/api/character/${e.target.value}`;
+  const res = await axios.get(url);
+  name.innerText = res.data.name;
+  image.src = res.data.image;
+  status.innerHTML = `<b>Status:</b> ${res.data.status}`;
+  place.innerHTML = `<b>Location: ${res.data.location.name}`;
+  title.innerText = res.data.name;
 })
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const comment = document.querySelector("#comment");
+  const li = document.createElement("li");
+  li.innerHTML = `<b>${title.textContent}:</b> ${comment.value}`
+  comments.appendChild(li)
+  comment.value = ""
 
-
-// li.addEventListener("click", () => {
-//     const mainTag = document.createElement("main")
-// const body = document.querySelector("body")
-// body.appendChild(mainTag)
-// }
+})
